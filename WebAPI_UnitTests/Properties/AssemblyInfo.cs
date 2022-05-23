2235a04d7a8255d4515709df514967bb488a98a9
KevinPongo <messinokev@gmail.com>
2022-05-23 18:37:10 +0200

    test commit

---
+++ WebAPI_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebAPI_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAPI_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e31c81d-bdff-42f5-a72c-8825cc037de3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebAPI_UnitTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+namespace WebAPI_UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestSuccess1()
+        {
+        }
+
+        [Fact]
+        public void TestSuccess2()
+        {
+        }
+
+        [Theory]
+        [InlineData("https://www.google.com")]
+        [InlineData("fffaaa")]
+        public void TestMethod1(string uri)
+        {
+            new Uri(uri);
+        }
+    }
+}
